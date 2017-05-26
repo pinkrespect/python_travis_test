a630c27ec4656c5537f7e3219d0618175fa9b1ba
Maisie <miss.sonagi@gmail.com>
2017-05-26 15:53:32 +0900

    Edit test.py

--- test.py
+++ test.py
@@ -3,29 +3,32 @@ class Encoder:
     def __init__(self, array):
         self.divisor = [1, 1, 0, 1, 0, 1]
         self.dividend = list(array) + [0] * (len(self.divisor) - 1)
-    
+        self.array = list(array)
+
     @staticmethod
     def XOR(number1, number2):
         return 0 if number1 is number2 else 1
 
-    def calculate(self, array):
+    def calculate(self):
         codeword = [0] * len(self.dividend)
+        
         for i in range(len(self.dividend)-len(self.divisor)):
             if self.dividend[i] is 1:
                 for j in range(len(self.divisor)):
                     self.dividend[i+j] = Encoder.XOR(self.divisor[j],
                                                      self.dividend[i+j])
 
-        for i in range(len(array)):
-            codeword[i] = array[i]
+        for i in range(len(self.array)):
+            codeword[i] = self.array[i]
 
         for i in range(len(self.divisor) - 1):
-            codeword.append(self.dividend[i + len(array)])
+            codeword.append(self.dividend[i + len(self.array)])
 
         return codeword
 
 
-encoder = Encoder([1, 0, 1, 0, 0, 0, 1, 1, 0, 1])
-CODEWORD = encoder.calculate([1, 0, 1, 0, 0, 0, 1, 1, 0, 1])
+data = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1]
+encoder = Encoder(data)
+CODEWORD = encoder.calculate()
 print("Codeword: ", CODEWORD)
 
