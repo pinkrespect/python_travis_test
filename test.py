8f77c854db292feff68b27b1f857d7a8921c544b
Maisie <miss.sonagi@gmail.com>
2017-05-26 16:33:01 +0900

    Edit test.py

--- test.py
+++ test.py
@@ -1,30 +1,25 @@
-
 def XOR(number1, number2):
     return 0 if number1 is number2 else 1
 
 
-def calculate(data):
+def calculate(array):
     divisor = [1, 1, 0, 1, 0, 1]
-    dividend = list(data) + [0] * (len(divisor) - 1)
-    array = list(data)
+    dividend = array + [0] * (len(divisor) - 1)
     codeword = [0] * len(array)
 
     for i in range(len(array)):
-        if dividend[i] is 1:
+        if dividend[i] == 1:
             for j, k in enumerate(divisor):
                 dividend[i+j] = XOR(k, dividend[i+j])
 
-    for i, j in enumerate(array):
-        codeword[i] = j
+    codeword = list(array)
 
     for i in range(len(divisor) - 1):
         codeword.append(dividend[i + len(array)])
 
-    print(len(codeword))
     return codeword
 
 
 one = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1]
 CODEWORD = calculate(one)
 print("Codeword: ", CODEWORD)
-
