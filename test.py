75a6ab027db59dd4b135edc2818082dddc6cc95a
Maisie <miss.sonagi@gmail.com>
2017-05-26 16:22:30 +0900

    Edit test.py

--- test.py
+++ test.py
@@ -1,34 +1,30 @@
-class Encoder:
-    def __init__(self, array):
-        self.divisor = [1, 1, 0, 1, 0, 1]
-        self.dividend = list(array) + [0] * (len(self.divisor) - 1)
-        self.array = list(array)
 
-    @staticmethod
-    def XOR(number1, number2):
-        return 0 if number1 is number2 else 1
-
-    def calculate(self):
-        dividend = list(self.dividend)
-        codeword = [0] * len(self.array)
-
-        for i in range(len(self.array)):
-            if dividend[i] is 1:
-                for j in range(len(self.divisor)):
-                    dividend[i+j] = Encoder.XOR(self.divisor[j], dividend[i+j])
-
-        for i in range(len(self.array)):
-            codeword[i] = self.array[i]
-
-        for i in range(len(self.divisor) - 1):
-            codeword.append(dividend[i + len(self.array)])
-
-        print(len(codeword))
-        return codeword
+def XOR(number1, number2):
+    return 0 if number1 is number2 else 1
 
 
-data = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1]
-encoder = Encoder(data)
-CODEWORD = encoder.calculate()
+def calculate(data):
+    divisor = [1, 1, 0, 1, 0, 1]
+    dividend = list(data) + [0] * (len(divisor) - 1)
+    array = list(data)
+    codeword = [0] * len(array)
+
+    for i in range(len(array)):
+        if dividend[i] is 1:
+            for j, k in enumerate(divisor):
+                dividend[i+j] = XOR(k, dividend[i+j])
+
+    for i, j in enumerate(array):
+        codeword[i] = j
+
+    for i in range(len(divisor) - 1):
+        codeword.append(dividend[i + len(array)])
+
+    print(len(codeword))
+    return codeword
+
+
+one = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1]
+CODEWORD = calculate(one)
 print("Codeword: ", CODEWORD)
 
